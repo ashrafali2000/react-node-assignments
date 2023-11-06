daeacc327dc4a2d50c5f566574a7bc310c403ce3
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-11-06 16:30:01 +0500

    import addProducts frontend

--- products-front/src/App.js
+++ products-front/src/App.js
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react"
 import productUrl from "./axios"
 import {Card} from "antd"
+import AddProduct from "./newProducts";
 function App() {
   const [product, setProduct] = useState([]);
 
@@ -8,9 +9,13 @@ function App() {
     productUrl.get("/products").then(res => setProduct(res.data.products));
   }, [])
   
+  const addProductHandler = () => {
+
+  }
   return (
     <div className="App">
-      {product.map(p => <Card key={p.pId}> {p.title}</Card> )}
+      {/* {product.map(p => <Card key={p.pId}> {p.title}</Card> )} */}
+      <AddProduct addProductHandler = {addProductHandler}></AddProduct>
     </div>
   );
 }
