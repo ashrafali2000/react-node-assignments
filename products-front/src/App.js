00ec79ad3f0fe0a50d192d0ea04a73fb3cd92f3c
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-11-06 16:21:26 +0500

    render all products

--- products-front/src/App.js
+++ products-front/src/App.js
@@ -1,8 +1,16 @@
-
+import {useEffect, useState} from "react"
+import productUrl from "./axios"
+import {Card} from "antd"
 function App() {
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    productUrl.get("/products").then(res => setProduct(res.data.products));
+  }, [])
+  
   return (
     <div className="App">
-      
+      {product.map(p => <Card key={p.pId}> {p.title}</Card> )}
     </div>
   );
 }
