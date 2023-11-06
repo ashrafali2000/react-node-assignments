f3536dad633d6931bac169a55a4b5864d1cdf271
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-11-06 16:22:00 +0500

    require cors in backend

--- products-backend/app.js
+++ products-backend/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const PORT = 8000;
-const products = require("./routes/product")
+const products = require("./routes/product");
 
+app.use(cors());
 
 
 app.use("/products",products);
