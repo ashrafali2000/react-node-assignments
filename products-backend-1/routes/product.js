bc54f07bcf1fe5f238ecc594651f53b8b578806c
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-11-07 15:56:56 +0500

    post request for product

--- products-backend-1/routes/product.js
+++ products-backend-1/routes/product.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const path  = require("path")
 const router = express.Router();
-const products = require("../data/product.json")
+const product = require("../data/product.json");
+const { addProduct } = require("../model/product");
 
 
 router.get("/", (req, res) => {
-    res.send(products);
+    res.send(product);
+})
+router.post("/", (req, res) => {
+  const {title, price, imgUrl} = req.body;
+  addProduct(title, price, imgUrl);
+    res.send();
 })
 
 module.exports = router;
