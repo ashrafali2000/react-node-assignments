42ed167723ea7bef651dd297436d817b3395083d
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-11-08 16:31:03 +0500

    wrap navbar into ProviderContext

--- products-frontend-1/src/App.js
+++ products-frontend-1/src/App.js
@@ -3,17 +3,32 @@ import Navbar from "./components/navbar";
 import Home from "./pages/home";
 import SignIn from "./pages/signIn";
 import SignUp from "./pages/signUp";
+import ProductContext from "./context/productCtx";
+import { useState } from "react";
+import SingleProduct from "./pages/singleProduct";
 
 function App() {
 
-  
+  const [product, setProduct] = useState([]);
+const chosenProduct = (data) => [
+ setProduct(data)
+]
+
   return (
     <div className="App">
        <Routes>
-        <Route path="/" element={<Navbar></Navbar>}>
+         
+        <Route path="/" element={
+          <ProductContext.Provider value={{
+            product : product, chosenProduct
+          }}>
+            <Navbar></Navbar>
+          </ProductContext.Provider>
+        }>
          <Route index element={<Home></Home>}/>
          <Route path="/login" element={<SignIn></SignIn>}/>
          <Route path="/signup" element={<SignUp></SignUp>}/>
+         <Route path="/electronics" element={<SingleProduct product = {product}></SingleProduct>}/>
         </Route>
        </Routes>
     </div>
