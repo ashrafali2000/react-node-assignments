f8158439842f6fd55c4c1a54310ba3f6b4aae6dc
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-11-11 16:28:14 +0500

    create admin route

--- products-frontend-1/src/App.js
+++ products-frontend-1/src/App.js
@@ -6,6 +6,7 @@ import SignUp from "./pages/signUp";
 import ProductContext from "./context/productCtx";
 import { useState } from "react";
 import SingleProduct from "./pages/singleProduct";
+import Admin from "./pages/admin";
 
 function App() {
 
@@ -28,6 +29,7 @@ const chosenProduct = (data) => [
          <Route index element={<Home></Home>}/>
          <Route path="/login" element={<SignIn></SignIn>}/>
          <Route path="/signup" element={<SignUp></SignUp>}/>
+         <Route path="/admin" element={<Admin></Admin>}/>
          <Route path="/electronics" element={<SingleProduct product = {product}></SingleProduct>}/>
         </Route>
        </Routes>
--- products-frontend-1/src/components/navbar/index.js
+++ products-frontend-1/src/components/navbar/index.js
@@ -194,6 +194,12 @@ export default function Navbar() {
             >
               Company
             </a>
+            <a
+              href="/admin"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Admin
+            </a>
           </Popover.Group>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
             <a
